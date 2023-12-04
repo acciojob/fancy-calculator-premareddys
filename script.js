6bc7c2b5b3d4480acfa40e39f5c3d7064de0a555
Siddareddygari Prema Reddy <92963246+premareddys@users.noreply.github.com>
2023-12-04 18:28:51 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
 
-let num1= promt("enter the second number:");
+function myFunction(){
+let num1= promt("enter the first number:");
+	console.log(+num1);
 let num2= promt("enter the second number:");
-const mul = num1 * 2;
-const result = mul + num2;
-alert("The final result is:" +result);
+	console.log(+num2);
+	let mul=0;
+	let result=0;
+	if(num1>0 && num2>0){
+      mul = num1 * 2;
+     result = mul + num2;
+	}
+ alert("The final result is:" +result);
+};
 
 
